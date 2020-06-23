0985cb303463e6e8b2f067d1c3af2e9cfaf1e421
Shreya Rajkumar <shreyajerry@gmail.com>
2020-06-23 18:18:19 -0400

    NX Therapy

---
+++ services/curation.js
@@ -0,0 +1,244 @@
+/**
+ * Copyright 2019-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * Messenger For Original Coast Clothing
+ * https://developers.facebook.com/docs/messenger-platform/getting-started/sample-apps/original-coast-clothing
+ */
+
+"use strict";
+
+// Imports dependencies
+const Response = require("./response"),
+  config = require("./config"),
+  i18n = require("../i18n.config");
+
+module.exports = class Curation {
+  constructor(user, webhookEvent) {
+    this.user = user;
+    this.webhookEvent = webhookEvent;
+  }
+
+  handlePayload(payload) {
+    let response;
+    let outfit;
+
+    switch (payload) {
+      case "SUMMER_COUPON":
+        response = [
+          Response.genText(
+            i18n.__("leadgen.promo", {
+              userFirstName: this.user.firstName
+            })
+          ),
+          Response.genGenericTemplate(
+            `${config.appUrl}/coupon.png`,
+            i18n.__("leadgen.title"),
+            i18n.__("leadgen.subtitle"),
+            [Response.genPostbackButton(i18n.__("leadgen.apply"), "COUPON_50")]
+          )
+        ];
+        break;
+
+      case "COUPON_50":
+        outfit = `${this.user.gender}-${this.randomOutfit()}`;
+
+        response = [
+          Response.genText(i18n.__("leadgen.coupon")),
+          Response.genGenericTemplate(
+            `${config.appUrl}/styles/${outfit}.jpg`,
+            i18n.__("curation.title"),
+            i18n.__("curation.subtitle"),
+            [
+              Response.genWebUrlButton(
+                i18n.__("curation.shop"),
+                `${config.shopUrl}/products/${outfit}`
+              ),
+              Response.genPostbackButton(
+                i18n.__("curation.show"),
+                "CURATION_OTHER_STYLE"
+              ),
+              Response.genPostbackButton(
+                i18n.__("curation.sales"),
+                "CARE_SALES"
+              )
+            ]
+          )
+        ];
+        break;
+
+      case "CURATION":
+        response = Response.genQuickReply(i18n.__("curation.prompt"), [
+          {
+            title: i18n.__("curation.me"),
+            payload: "CURATION_FOR_ME"
+          },
+          {
+            title: i18n.__("curation.someone"),
+            payload: "CURATION_SOMEONE_ELSE"
+          }
+        ]);
+        break;
+
+      case "CURATION_FOR_ME":
+        response = generateInspiringQuote();
+        break;
+      case "CURATION_SOMEONE_ELSE":
+        response = generateFunnyQuote();
+        break;
+
+      case "CURATION_OCASION_WORK":
+        // Store the user budget preference here
+        response = Response.genQuickReply(i18n.__("curation.price"), [
+          {
+            title: "~ $20",
+            payload: "CURATION_BUDGET_20_WORK"
+          },
+          {
+            title: "~ $30",
+            payload: "CURATION_BUDGET_30_WORK"
+          },
+          {
+            title: "+ $50",
+            payload: "CURATION_BUDGET_50_WORK"
+          }
+        ]);
+        break;
+
+      case "CURATION_OCASION_DINNER":
+        // Store the user budget preference here
+        response = Response.genQuickReply(i18n.__("curation.price"), [
+          {
+            title: "~ $20",
+            payload: "CURATION_BUDGET_20_DINNER"
+          },
+          {
+            title: "~ $30",
+            payload: "CURATION_BUDGET_30_DINNER"
+          },
+          {
+            title: "+ $50",
+            payload: "CURATION_BUDGET_50_DINNER"
+          }
+        ]);
+        break;
+
+      case "CURATION_OCASION_PARTY":
+        // Store the user budget preference here
+        response = Response.genQuickReply(i18n.__("curation.price"), [
+          {
+            title: "~ $20",
+            payload: "CURATION_BUDGET_20_PARTY"
+          },
+          {
+            title: "~ $30",
+            payload: "CURATION_BUDGET_30_PARTY"
+          },
+          {
+            title: "+ $50",
+            payload: "CURATION_BUDGET_50_PARTY"
+          }
+        ]);
+        break;
+
+      case "CURATION_BUDGET_20_WORK":
+      case "CURATION_BUDGET_30_WORK":
+      case "CURATION_BUDGET_50_WORK":
+      case "CURATION_BUDGET_20_DINNER":
+      case "CURATION_BUDGET_30_DINNER":
+      case "CURATION_BUDGET_50_DINNER":
+      case "CURATION_BUDGET_20_PARTY":
+      case "CURATION_BUDGET_30_PARTY":
+      case "CURATION_BUDGET_50_PARTY":
+        response = this.genCurationResponse(payload);
+        break;
+
+      case "CURATION_OTHER_STYLE":
+        // Build the recommendation logic here
+        outfit = `${this.user.gender}-${this.randomOutfit()}`;
+
+        response = Response.genGenericTemplate(
+          `${config.appUrl}/styles/${outfit}.jpg`,
+          i18n.__("curation.title"),
+          i18n.__("curation.subtitle"),
+          [
+            Response.genWebUrlButton(
+              i18n.__("curation.shop"),
+              `${config.shopUrl}/products/${outfit}`
+            ),
+            Response.genPostbackButton(
+              i18n.__("curation.show"),
+              "CURATION_OTHER_STYLE"
+            )
+          ]
+        );
+        break;
+    }
+
+    return response;
+  }
+
+  genCurationResponse(payload) {
+    let occasion = payload.split("_")[3].toLowerCase();
+    let budget = payload.split("_")[2].toLowerCase();
+    let outfit = `${this.user.gender}-${occasion}`;
+
+    let buttons = [
+      Response.genWebUrlButton(
+        i18n.__("curation.shop"),
+        `${config.shopUrl}/products/${outfit}`
+      ),
+      Response.genPostbackButton(
+        i18n.__("curation.show"),
+        "CURATION_OTHER_STYLE"
+      )
+    ];
+
+    if (budget === "50") {
+      buttons.push(
+        Response.genPostbackButton(i18n.__("curation.sales"), "CARE_SALES")
+      );
+    }
+
+    let response = Response.genGenericTemplate(
+      `${config.appUrl}/styles/${outfit}.jpg`,
+      i18n.__("curation.title"),
+      i18n.__("curation.subtitle"),
+      buttons
+    );
+
+    return response;
+  }
+
+  randomOutfit() {
+    let occasion = ["work", "party", "dinner"];
+    let randomIndex = Math.floor(Math.random() * occasion.length);
+
+    return occasion[randomIndex];
+  }
+
+  generateInspiringQuote() {
+    let inspiringQuotes = ["No one can make you feel inferior without your consent.", "We are all in the gutter, but some of us are looking at the stars.", 
+    "I have not failed. I've just found 10,000 ways that won't work.", "It is never too late to be what you might have been.",
+  "Everything you can imagine is real.", "Life isn't about finding yourself. Life is about creating yourself.",
+"Do what you can, with what you have, where you are.", "Success is not final, failure is not fatal: it is the courage to continue that counts",
+"Yesterday is history, tomorrow is a mystery, today is a gift of God, which is why we call it the present.", "Imperfection is beauty, madness is genius and it's better to be absolutely ridiculous than absolutely boring."];
+    let randomIndex = Math.floor(Math.random() * inspiringQuote.length);
+
+    return inspiringQuote[randomIndex];
+  }
+
+  generateFunnyQuote() {
+    let funnyQuotes = ["Have you noticed that all the people in favor of birth control are already born", "The surest sign that intelligent life exists elsewhere in the universe is that it has never tried to contact us.", 
+  "If you’re going to tell people the truth, be funny or they’ll kill you.", "We never really grow up, we only learn how to act in public.",
+"As a child my family’s menu consisted of two choices: take it or leave it.", "My favorite machine at the gym is the vending machine.",
+"He who laughs last didn’t get the joke", "I always arrive late at the office, but I make up for it by leaving early.",
+"Political correctness is tyranny with manners.", "Remember, today is the tomorrow you worried about yesterday."];
+    let randomIndex = Math.floor(Math.random() * funnyQuotes.length);
+
+    return funnyQuotes[randomIndex];
+  }
+ 
+};
